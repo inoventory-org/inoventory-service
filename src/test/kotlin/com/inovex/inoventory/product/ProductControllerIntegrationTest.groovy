b227d027395e4f5c57b3b27af5bea19daa2ae11d
Rudy Ailabouni <rudy.ailabouni@inovex.de>
2022-12-15 14:46:02 +0100

    add integration test base

---
+++ src/test/kotlin/com/inovex/inoventory/product/ProductControllerIntegrationTest.groovy
@@ -0,0 +1,16 @@
+package com.inovex.inoventory.product
+
+
+class ProductControllerIntegrationTest extends spock.lang.Specification {
+    void setup() {
+    }
+
+    void cleanup() {
+    }
+
+    def "GetAll"() {
+    }
+
+    def "Post"() {
+    }
+}
