de30e02e61967ad369e47c589e817eaba719b13c
Rudy Ailabouni <rudy.ailabouni@inovex.de>
2022-12-16 14:30:05 +0100

    test pipeline update image

--- src/main/kotlin/com/inovex/inoventory/product/ProductService.kt
+++ src/main/kotlin/com/inovex/inoventory/product/ProductService.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service
 @Service
 class ProductService(private val repository: ProductRepository) {
 
-    fun getAll() = listOf<Product>(Product(0,"Statically Created Product"))
+    fun getAll() = listOf<Product>(Product(0,"Image Update Done Automatically"))
 //    fun getAll() = repository.findAll()
 
     fun create(product: Product): Product {
