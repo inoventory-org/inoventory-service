0a55373252205d7f33a118b2625f0a4bd7b38770
Rudy Ailabouni <rudy.ailabouni@inovex.de>
2022-12-16 14:56:34 +0100

    undo temp changes and update deploy stage

--- src/main/kotlin/com/inovex/inoventory/product/ProductService.kt
+++ src/main/kotlin/com/inovex/inoventory/product/ProductService.kt
@@ -6,10 +6,9 @@ import org.springframework.stereotype.Service
 @Service
 class ProductService(private val repository: ProductRepository) {
 
-    fun getAll() = listOf<Product>(Product(0,"Image Update Done Automatically"))
-//    fun getAll() = repository.findAll()
+    fun getAll() = repository.findAll()
 
     fun create(product: Product): Product {
-        return repository.save(product);
+        return repository.save(product)
     }
 }
