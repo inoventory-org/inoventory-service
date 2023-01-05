c5fd692fc591213080a5cb220967c67914bf17ea
Sören Uebis <soeren.uebis@inovex.de>
2023-01-05 16:35:53 +0100

    cleanup & dependency update

--- src/main/kotlin/com/inovex/inoventory/product/tag/TagService.kt
+++ src/main/kotlin/com/inovex/inoventory/product/tag/TagService.kt
@@ -8,6 +8,6 @@ class TagService(private val repository: TagRepository) {
     fun getAll() : List<TagEntity> = repository.findAll()
 
     fun create(tag: TagEntity): TagEntity {
-        return repository.save(tag);
+        return repository.save(tag)
     }
 }
