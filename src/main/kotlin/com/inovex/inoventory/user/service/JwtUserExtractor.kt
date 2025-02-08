247de84e12a0a04765a57384e3a41cf74595f778
Sören Uebis <soeren.uebis@gmail.com>
2025-02-08 16:28:09 +0100

    use MockUserExtractor for local setup

--- src/main/kotlin/com/inovex/inoventory/user/service/JwtUserExtractor.kt
+++ src/main/kotlin/com/inovex/inoventory/user/service/JwtUserExtractor.kt
@@ -1,12 +1,14 @@
 package com.inovex.inoventory.user.service
 
 import com.inovex.inoventory.user.dto.UserDto
+import org.springframework.context.annotation.Profile
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
 import org.springframework.stereotype.Component
 import java.util.*
 
 @Component
+@Profile("!local")
 class JwtUserExtractor : UserDetailsExtractor {
     override fun extractUser(): UserDto {
         val claims = (SecurityContextHolder.getContext().authentication as JwtAuthenticationToken).token.claims
