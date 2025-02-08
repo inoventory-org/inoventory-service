710d644f92c9fc896a3878c61029f54b3ba3e401
Sören Uebis <soeren.uebis@gmail.com>
2025-02-08 16:10:58 +0100

    disable security when running locally

--- src/main/kotlin/com/inovex/inoventory/config/SecurityConfig.kt
+++ src/main/kotlin/com/inovex/inoventory/config/SecurityConfig.kt
@@ -2,12 +2,14 @@ package com.inovex.inoventory.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
 
 
 @Configuration
+@Profile("!local")
 class SecurityConfig {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
@@ -15,6 +17,18 @@ class SecurityConfig {
             it.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .anyRequest().authenticated()
         }.oauth2ResourceServer { it.jwt(Customizer.withDefaults()) }
+        return http.build()
+    }
+}
+
+@Configuration
+@Profile("local")
+class SecurityConfigLocal {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests { it.anyRequest().permitAll() }
+            .csrf { it.disable() }
         return http.build()
     }
 }
